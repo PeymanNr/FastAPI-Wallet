a7c68019d44bf2b4f2005bc59bbca028ac052dca
PeymanNr <peyman.naseri7@gmail.com>
2025-05-21 13:02:28 +0330

    Add env file for config

--- db/database.py
+++ db/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from databases import Database
 from sqlalchemy.orm import DeclarativeBase
+from config import settings
 
 
-DATABASE_URL = "postgresql+asyncpg://ipeymani:932319361@localhost:5432/fastapiwallet"
+DATABASE_URL = settings.DATABASE_URL
 
 database = Database(DATABASE_URL)
 engine = create_async_engine(DATABASE_URL, echo=True)
